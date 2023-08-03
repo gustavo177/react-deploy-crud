e8e68d711546baf6316fbdcbc1534fcb51d6f642
gustavo177 <gtvadf177@hotmail.com>
2023-08-03 11:03:32 -0500

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    // console.log(tasks);
+    // console.log(taskId);
+
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  // let x = 20;
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
